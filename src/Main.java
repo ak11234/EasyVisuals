2bc0f1efcd7f8e59fb505b7bdbc5b78bf66befc0
ak11234 <akpc556@gmail.com>
2016-07-13 13:03:53 -0500

    first commit
    first commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Adam Kalman on 7/6/2016. EasyVisuals Default Functions
+ */
+public class Main {
+    public static void main(String[] args){
+        EasyVisuals sound = new EasyVisuals();
+        sound.quickStart();
+    }
+}
